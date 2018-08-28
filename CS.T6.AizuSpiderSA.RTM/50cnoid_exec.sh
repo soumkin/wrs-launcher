6f061767e9e636ee30fbc654a426dd3f96c9778f
Kin-ichi WATANABE <kin@soum.co.jp>
2018-08-28 12:34:44 +0900

    adds launch button T1..T6 sample scripts.

---
+++ CS.T6.AizuSpiderSA.RTM/50cnoid_exec.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+. ${HOME}/.profile
+
+#. `basename $0`.conf
+_current_dir="`pwd`"
+
+_project_dir="${HOME}/work/Choreonoid/choreonoid.git/sample/WRS2018"
+_project_file="T6-AizuSpiderSA-RTM.py"
+
+###
+choreonoid --python ${_project_dir}/${_project_file}
