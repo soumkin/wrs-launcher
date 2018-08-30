ebd4272a8bdfc845326bb735cee1eba4a342c1d5
Kin-ichi WATANABE <kin@soum.co.jp>
2018-08-30 12:07:53 +0900

    adds Sample1(OC)

---
+++ OC.RTM.AizuSpider.Sample1/50cnoid_exec.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+. ${HOME}/.profile
+
+#. `basename $0`.conf
+_current_dir="`pwd`"
+
+_project_dir="${_current_dir}"
+_project_file="OpenRTM-AizuSpider-Terminal-Sample1.cnoid"
+
+###
+choreonoid ${_project_dir}/${_project_file}
