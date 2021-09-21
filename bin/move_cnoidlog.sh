de474bdd52f0d3ca50e7cdaaad18d72076b8d32e
Kensuke Shibata <kshibata.ggac@soum.co.jp>
2021-09-21 16:24:01 +0900

    mod choreonoid log move script.

--- bin/move_cnoidlog.sh
+++ bin/move_cnoidlog.sh
@@ -8,7 +8,7 @@ echo ">>>>> start move choreonoid log <<<<<"
 _var="`basename $PWD`"
 
 #
-find $CNOID_PROJ_DIR/$_var-*.log -type f | xargs -I% mv -v % ./
+find $CNOID_PROJ_DIR/$_var*.log -type f | xargs -I% mv -v % ./
 
 exit 0
 
